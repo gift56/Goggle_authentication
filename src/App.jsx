c0a528032a298fe22d37bf0a40014fae8e1776de
gift56 <efeasiughu@gmail.com>
2023-04-19 06:57:29 -0500

    done with how to do google authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import jwtDecode from "jwt-decode";
 
 export default function App() {
+  const [currentUser, setCurrentUser] = useState({});
+  const responseCallback = (res) => {
+    console.log("Encoded jwt token" + res.credential);
+    const userData = jwtDecode(res.credential);
+    setCurrentUser(userData);
+    document.getElementById("signinDiv").hidden = true;
+  };
+
+  useEffect(() => {
+    /* Google initailization */
+    google.accounts.id.initialize({
+      client_id: process.env.AUTH_KEY,
+      callback: responseCallback,
+    });
+    google.accounts.id.renderButton(document.getElementById("signinDiv"), {
+      theme: "outline",
+      size: "large",
+    });
+  }, []);
+
   return (
-    <div>App</div>
-  )
+    <div>
+      <div id="signinDiv"></div>
+      {currentUser && (
+        <div>
+          <img src={currentUser.picture} alt="" />
+          <h3>{currentUser.name}</h3>
+        </div>
+      )}
+    </div>
+  );
 }
